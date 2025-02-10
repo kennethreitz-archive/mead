938208bc6b4e0225705a0b5087701dad850f235f
Nasser <abdounasser202@gmail.com>
2025-02-10 22:16:44 +0100

    [FIX] Blog setup at launch

--- mead/views/setup.py
+++ mead/views/setup.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, current_app
-from mead.models import db, Settings, User
+from mead.models import db, Settings, User, Post
 from werkzeug.security import generate_password_hash
 
 setup = Blueprint("setup", __name__)
@@ -19,6 +19,7 @@ def setup_blog():
                 blog_name=request.form["blog_name"],
                 blog_description=request.form["blog_description"],
             )
+            db.session.add(settings)
 
             admin = User(
                 username=request.form["admin_username"],
@@ -26,12 +27,38 @@ def setup_blog():
                 password=generate_password_hash(request.form["admin_password"]),
                 role="admin",
             )
-
-            db.session.add(settings)
             db.session.add(admin)
             db.session.commit()
 
-            return redirect(url_for("index"))
+            # create default page content (Now and Projects)
+            # for more info about Now page -> https://now.page/
+            static_pages = [
+                {
+                    "title": "Now",
+                    "content":"""Describe what you are doing actually to keep
+                    your readers updated. For more info visit  https://now.page/
+                    """,
+                    "is_page":True
+                },
+                {
+                    "title": "Projects",
+                    "content": "Talk about your projects here",
+                    "is_page":True
+                }
+            ]
+
+            for page in static_pages:
+                pages = Post(
+                    title=page.get("title"),
+                    slug=page.get("title").lower(),
+                    content=page.get("content"),
+                    is_page=page.get("is_page"),
+                    author_id=admin.id
+                )
+                db.session.add(pages)
+                db.session.commit()
+
+            return redirect(url_for("blog.index"))
         except Exception as e:
             print(f"Setup error: {e}")
             db.session.rollback()
